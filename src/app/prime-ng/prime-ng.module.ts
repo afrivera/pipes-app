9d695a11cc03a5edcb0c26c3804ba32638f1beb4
afrivera <andresrfelip@hotmail.com>
2022-05-30 18:40:43 -0500

    create a primeNd Module

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// PrimeNg
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ButtonModule,
+    CardModule
+  ]
+})
+export class PrimeNgModule { }
